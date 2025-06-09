1be5f4c1f2294212dc89cb15c9acc170ceeb3856
Black Roland <mail@roland.black>
2025-06-09 05:19:49 +0300

    Conversation implementation based on https://github.com/home-assistant/core/commit/49a62d52947ff8e92f8e9e9921cd71e23acf84ca

--- custom_components/cloud_ru_ai/__init__.py
+++ custom_components/cloud_ru_ai/__init__.py
@@ -16,24 +16,45 @@
 
 from __future__ import annotations
 
+import openai
 from homeassistant.config_entries import ConfigEntry
-from homeassistant.const import Platform
+from homeassistant.const import CONF_API_KEY, Platform
 from homeassistant.core import HomeAssistant
+from homeassistant.exceptions import ConfigEntryNotReady
 from homeassistant.helpers import config_validation as cv
-from homeassistant.helpers.typing import ConfigType
+from homeassistant.helpers.httpx_client import get_async_client
 
-from .const import DOMAIN
+from .const import (CLIENT_API_KEY, CLIENT_BASE_URI, CLIENT_PROJECT_ID,
+                    CONF_PROJECT_ID, DOMAIN, LOGGER)
 
 PLATFORMS = (Platform.CONVERSATION,)
 CONFIG_SCHEMA = cv.config_entry_only_config_schema(DOMAIN)
 
-
-async def async_setup(hass: HomeAssistant, _config: ConfigType) -> bool:
-    return True
+type CloudRUAIConfigEntry = ConfigEntry[openai.AsyncClient]
 
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Set up Cloud.ru Foundation Models from a config entry."""
+    client = openai.AsyncOpenAI(
+        api_key=entry.data[CONF_API_KEY],
+        http_client=get_async_client(hass),
+        base_url=CLIENT_BASE_URI,
+        default_headers={CLIENT_API_KEY: entry.data[CONF_API_KEY], CLIENT_PROJECT_ID: entry.data[CONF_PROJECT_ID]}
+    )
+
+    # Cache current platform data which gets added to each request (caching done by library)
+    _ = await hass.async_add_executor_job(client.platform_headers)
+
+    try:
+        await hass.async_add_executor_job(client.with_options(timeout=10.0).models.list)
+    except openai.AuthenticationError as err:
+        LOGGER.error("Invalid API key: %s", err)
+        return False
+    except openai.OpenAIError as err:
+        raise ConfigEntryNotReady(err) from err
+
+    entry.runtime_data = client
+
     await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
 
     return True
